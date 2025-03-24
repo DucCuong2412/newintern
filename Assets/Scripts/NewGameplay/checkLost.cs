928fe997a532c4386a6d69ce31f2195c02d101c5
DucCuong2412 <162032280+DucCuong2412@users.noreply.github.com>
2025-03-25 00:50:34 +0700

    submid

---
+++ Assets/Scripts/NewGameplay/checkLost.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class checkLost : MonoBehaviour
+{
+    public static checkLost Instance;
+
+    private void Start()
+    {
+        StartCoroutine(waitforMinute());
+    }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator waitforMinute()
+    {
+        Debug.Log("đã hết 1 phút)");
+
+        yield return new WaitForSeconds(60);
+    }
+}
